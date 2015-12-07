927a6adb56ea5b9bb48edabb9628b701d42527ee
Brett Dunbar <bidunbar@email.arizona.edu>
2015-12-06 18:35:48 -0700

    submitting 2 files
    the start of the .java file and the sql commands to create the tables

---
+++ DatabaseController.java
@@ -0,0 +1,38 @@
+/**
+ *	DatabaseController.java --	This program is what connectes all our jsp files to
+ *	our database server. It establishes our connection and it will allow us to call
+ *  the methods needed to run our queries that include: insert, delete, update, and 
+ *  other queries specified in Prog4 specs.	
+ *
+ *  course: csc460
+ *	authors: Jorge Naranjo, Jason Royer, Brett Dunbar
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.sql.ResultSet;
+import java.util.*;
+
+public class DatabaseController {
+
+	/**
+	 * next few variables handles the connection, statement, username, and password
+	 */
+	static final long serialVersionUID = 1L;
+	protected Conenction conn;
+	protected Statement stmt;
+	protected String connectString = null;
+	protected String username = null, password = null;
+
+	// contructor to get the connection going
+	public DatabaseController() {
+		// username and password to connect to DB, change if needed
+		username = "bidunbar";
+		password = "a9730";
+		connectString = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+	} // end DatabaseController()
+
+	public openController 
+} // end DatabaseController
---
+++ extras/tables.sql
@@ -0,0 +1,80 @@
+create table test (
+	clientID number NOT NULL,
+	testType varchar2(5) check(testType in ('drive', 'write')) NOT NULL,
+	passed varchar2(1) check(passed in('y', 'n')) NOT NULL,
+	reason varchar2(200), 
+	testDate date NOT NULL,
+	FOREIGN KEY(clientID) REFERENCES bidunbar.client(clientID),
+	PRIMARY KEY(clientID)
+);
+
+create table client (
+	clientID number NOT NULL,
+	name varchar2(80) NOT NULL,
+	gender varchar2(1) check(gender in ('m', 'f')) NOT NULL,
+	address varchar2(80) NOT NULL,
+	city varchar2(80) NOT NULL,
+	phoneNum number check(phoneNum between 1111111111 and 9999999999) NOT NULL,
+	validLicense varchar2(1) check(validLicense in ('y', 'n')) NOT NULL,
+	PRIMARY KEY(clientID)
+);
+
+create table inteview (
+	empID number  NOT NULL,
+	clientID number  NOT NULL,
+	interviewDate date  NOT NULL,
+	needs varchar2(200)  NOT NULL,
+	validLicense varchar2(1) check(validLicense in ('y', 'n')) NOT NULL,
+	FOREIGN KEY(empID) REFERENCES bidunbar.employee(empID),
+	FOREIGN KEY(clientID) REFERENCES bidunbar.client(clientID),
+	PRIMARY KEY(empID, clientID)
+);
+
+create table lesson (
+	lessonNum number NOT NULL,
+	carID number NOT NULL,
+	clientID number NOT NULL,
+	officeID number NOT NULL,
+	fee number check(fee > 0) NOT NULL,
+	lessonDate date NOT NULL,
+	milesDriven number check(milesDriven > 0) NOT NULL,
+	FOREIGN KEY(carID) REFERENCES bidunbar.car(carID),
+	FOREIGN KEY(clientID) REFERENCES bidunbar.client(clientID),
+	FOREIGN KEY(officeID) REFERENCES bidunbar.office(officeID),
+	PRIMARY KEY(lessonNum)
+);
+
+create table employee (
+	empID number NOT NULL,
+	name varchar2(80) NOT NULL,
+	DOB date NOT NULL,
+	phoneNum number check(phoneNum between 1111111111 and 9999999999),
+	gender char NOT NULL,
+	jobTitle varchar2(80) NOT NULL,
+	carID number NOT NULL,
+	officeID number NOT NULL,
+	PRIMARY KEY(empID),
+	FOREIGN KEY(carID) REFERENCES bidunbar.car(carID),
+	FOREIGN KEY(officeID) REFERENCES bidunbar.office(officeID) 
+);
+
+create table car (
+	carID number NOT NULL,
+	milage number NOT NULL,
+	faults varchar2(1) check(faults in ('y', 'n')) NOT NULL,
+	empID number NOT NULL,
+	PRIMARY KEY(carID)
+);
+
+create table office (
+	officeID number NOT NULL,
+	officeName varchar2(80) NOT NULL,
+	managerID number NOT NULL,
+	phoneNum number check(phoneNum between 1111111111 and 9999999999),
+	address varchar2(80) NOT NULL,
+	city varchar2(80) NOT NULL,
+	state varchar2(80) NOT NULL,
+	PRIMARY KEY(officeID)
+	FOREIGN KEY(managerID) REFERENCES bidunbar.employee(empID)
+);
+
